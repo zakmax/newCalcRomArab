fd2139c78f14452f7e07889d8043790fb2fdb76c
zak-max15@yandex.ru <java180490>
2025-01-19 16:09:56 +0300

    Added code

--- src/code/Converter.java
+++ src/code/Converter.java
@@ -1,9 +1,13 @@
 package code;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
+
 class Converter {
     TreeMap<Character, Integer> romanKeyMap = new TreeMap<>();
     TreeMap<Integer, String> arabianKeyMap = new TreeMap<>();
+    Map<String, Integer> numberMap = new HashMap<>();
 
     public Converter() {
         romanKeyMap.put('I', 1);
@@ -14,22 +18,38 @@ class Converter {
         romanKeyMap.put('D', 500);
         romanKeyMap.put('M', 1000);
 
-        arabianKeyMap.put(1000, "M" );
-        arabianKeyMap.put(900, "CM" );
-        arabianKeyMap.put(500, "D" );
-        arabianKeyMap.put(400, "CD" );
-        arabianKeyMap.put(100, "C" );
-        arabianKeyMap.put(90, "XC" );
-        arabianKeyMap.put(50, "L" );
-        arabianKeyMap.put(40, "XL" );
-        arabianKeyMap.put(10, "X" );
-        arabianKeyMap.put(9, "IX" );
-        arabianKeyMap.put(5, "V" );
-        arabianKeyMap.put(4, "IV" );
-        arabianKeyMap.put(1, "I" );
+        arabianKeyMap.put(1000, "M");
+        arabianKeyMap.put(900, "CM");
+        arabianKeyMap.put(500, "D");
+        arabianKeyMap.put(400, "CD");
+        arabianKeyMap.put(100, "C");
+        arabianKeyMap.put(90, "XC");
+        arabianKeyMap.put(50, "L");
+        arabianKeyMap.put(40, "XL");
+        arabianKeyMap.put(10, "X");
+        arabianKeyMap.put(9, "IX");
+        arabianKeyMap.put(5, "V");
+        arabianKeyMap.put(4, "IV");
+        arabianKeyMap.put(1, "I");
+
+
+        numberMap.put("I", 1);
+        numberMap.put("II", 2);
+        numberMap.put("III", 3);
+        numberMap.put("IV", 4);
+        numberMap.put("V", 5);
+        numberMap.put("VI", 6);
+        numberMap.put("VII", 7);
+        numberMap.put("VIII", 8);
+        numberMap.put("IX", 9);
+        numberMap.put("X", 10);
+
 
     }
-    boolean isRoman(String number){ return romanKeyMap.containsKey(number.charAt(0));}
+
+    boolean isRoman(String number) {
+        return numberMap.containsKey(number);
+    }
 
     String intToRoman(int number) {
         StringBuilder roman = new StringBuilder();
@@ -42,20 +62,21 @@ class Converter {
         return roman.toString();
     }
 
-    public  int romanToInt(String s) {
+    public int romanToInt(String s) {
         int end = s.length() - 1;
         char[] arr = s.toCharArray();
         int arabian;
         int result = romanKeyMap.get(arr[end]);
-        for (int i = end - 1;  i >= 0; i--) {
+        for (int i = end - 1; i >= 0; i--) {
             arabian = romanKeyMap.get(arr[i]);
 
-            if(arabian < romanKeyMap.get(arr[i+1])) {
+            if (arabian < romanKeyMap.get(arr[i + 1])) {
                 result -= arabian;
-            }else {
-                result += arabian;            }
+            } else {
+                result += arabian;
+            }
         }
 
-        return  result;
+        return result;
     }
 }
